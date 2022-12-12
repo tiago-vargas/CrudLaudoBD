14c0c87e409dd14a2ce2c82d8400e9a54d6a3e99
Tiago Vargas <tiago.ce10@outlook.com>
2022-12-12 01:54:50 -0300

    Add TesteUnidadeMedida

---
+++ src/main/java/br/com/frota/util/TesteUnidadeMedida.java
@@ -0,0 +1,43 @@
+package br.com.frota.util;
+
+import br.com.frota.DAO.UnidadeMedidaDAO;
+import br.com.frota.model.UnidadeMedida;
+import br.com.frota.servico.ServicoUnidadeMedida;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class TesteUnidadeMedida {
+    static UnidadeMedidaDAO unidadeMedidaDAO = new UnidadeMedidaDAO();
+
+    static ServicoUnidadeMedida servicoUnidadeMedida = new ServicoUnidadeMedida();
+
+    public static void main(String[] args) throws SQLException {
+
+        //count
+        System.out.println(unidadeMedidaDAO.count());
+
+        //salvar
+        UnidadeMedida unidadeMedida = new UnidadeMedida("J/s");
+        servicoUnidadeMedida.salvar(unidadeMedida);
+        long id = unidadeMedida.getId();
+
+        //buscar por ID
+        unidadeMedida = unidadeMedidaDAO.findById(id);
+        System.out.println(unidadeMedida);
+
+        //Update
+        unidadeMedida.setDescricao("W");
+        unidadeMedidaDAO.updateUnidadeMedida(unidadeMedida);
+        unidadeMedida = unidadeMedidaDAO.findById(id);
+        System.out.println(unidadeMedida);
+
+        //Select all
+        List<UnidadeMedida> unidadeMedidas = unidadeMedidaDAO.selectAllUnidadeMedida();
+        unidadeMedidas.forEach(System.out::println);
+
+        //Delete
+        unidadeMedidaDAO.deleteUnidadeMedida(id);
+        unidadeMedidaDAO.selectAllUnidadeMedida().forEach(System.out::println);
+    }
+}
