18fab59bb22115400f79fa7e385b0f87b7930d86
Tiago Vargas <tiago.ce10@outlook.com>
2022-12-12 02:04:30 -0300

    Add TesteTipoExame

---
+++ src/main/java/br/com/frota/util/TesteTipoExame.java
@@ -0,0 +1,44 @@
+package br.com.frota.util;
+
+import br.com.frota.DAO.TipoExameDAO;
+import br.com.frota.model.TipoExame;
+import br.com.frota.servico.ServicoTipoExame;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class TesteTipoExame {
+    static TipoExameDAO tipoExameDAO = new TipoExameDAO();
+
+    static ServicoTipoExame servicoTipoExame = new ServicoTipoExame();
+
+    public static void main(String[] args) throws SQLException {
+
+        //count
+        System.out.println(tipoExameDAO.count());
+
+        //salvar
+        TipoExame tipoExame = new TipoExame("Descricao", "Observacao");
+        servicoTipoExame.salvar(tipoExame);
+        long id = tipoExame.getId();
+
+        //buscar por ID
+        tipoExame = tipoExameDAO.findById(id);
+        System.out.println(tipoExame);
+
+        //Update
+        tipoExame.setDescricao("Descricao-2");
+        tipoExame.setObservacao("Observacao-2");
+        tipoExameDAO.updateTipoExame(tipoExame);
+        tipoExame = tipoExameDAO.findById(id);
+        System.out.println(tipoExame);
+
+        //Select all
+        List<TipoExame> tipoExames = tipoExameDAO.selectAllTipoExame();
+        tipoExames.forEach(System.out::println);
+
+        //Delete
+        tipoExameDAO.deleteTipoExame(id);
+        tipoExameDAO.selectAllTipoExame().forEach(System.out::println);
+    }
+}
